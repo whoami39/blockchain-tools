d6abdbaa90bfa4c0a1313c569a63214c267e5162
whoami39 <whoami39@users.noreply.github.com>
2024-08-27 04:22:36 +0000

    deploy: 3426d42d5ddd336399077e213e5812e7e76281f8

---
+++ aleo/oula/prover/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --account) ACCOUNT=$2; shift 2 ;;
+        --pool) POOL=$2; shift 2 ;;
+        --name) WORKER_NAME=$2; shift 2 ;;
+        *) REST_ARGS="$REST_ARGS $1"; shift ;;
+    esac
+done
+
+if [ -z "$ACCOUNT" ]; then
+    echo "--account param empty"
+    exit 1
+fi
+
+if [ -z "$POOL" ]; then
+    POOL="wss://aleo.oula.network:6666"
+fi
+
+if [ -z "$WORKER_NAME" ]; then
+    WORKER_NAME=$(hostname)
+fi
+
+PROVER_PARAMS=" --pool $POOL --account $ACCOUNT --worker-name $WORKER_NAME"
+
+echo
+echo
+echo "Parameters: \`$PROVER_PARAMS\`"
+echo
+echo
+echo "- Telegram: https://t.me/blockchain_minter"
+echo "- Github: https://github.com/whoami39/blockchain-tools/tree/main/oula/aleo-miner"
+echo
+
+/app/oula-aleo-prover $PROVER_PARAMS $REST_ARGS
