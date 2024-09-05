d6444ed01dd5c3163778fce575b72d54c02a8416
whoami39 <whoami39@users.noreply.github.com>
2024-09-05 07:37:44 +0000

    deploy: 5a97825a204e5831851b83f30c96023919c24e81

--- aleo/f2pool/prover/entrypoint.sh
+++ aleo/f2pool/prover/entrypoint.sh
@@ -31,24 +31,24 @@ get_available_gpus() {
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -u) POOL=$2; shift 2 ;;
-        -w) WORKER=$2; shift 2 ;;
         -d) GPU=$2; shift 2 ;;
         *) REST_ARGS="$REST_ARGS $1"; shift ;;
     esac
 done
 
 POOL=${POOL:-"stratum+tcp://aleo-asia.f2pool.com:4400"}
-WORKER=${WORKER:-"whoami39.$(hostname | md5sum | cut -c1-8))"}
+ACCOUNT=${ACCOUNT:-"whoami39"}
+WORKER=${WORKER:-"$(hostname | md5sum | cut -c1-8)"}
 GPU=${GPU:-"$(get_available_gpus)"}
 
-PROVER_PARAMS="-u $POOL -w $WORKER -d $GPU"
+PROVER_PARAMS="-u $POOL -w $ACCOUNT.$WORKER -d $GPU"
 
 echo
 echo
 echo "Parameters: \`$PROVER_PARAMS\`"
 echo
 echo "- Telegram: https://t.me/blockchain_minter"
-echo "- Github: https://github.com/whoami39/blockchain-tools/tree/main/cysic/aleo-miner"
+echo "- Github: https://github.com/whoami39"
 echo
 echo
 
